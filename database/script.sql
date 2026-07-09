0303e626decff069d3c9da02be2624e145c3fcb1
Erik Souza <eriksouzalopes@gmail.com>
2026-07-09 00:30:24 -0300

    Estrutura inicial e script do banco de dados

---
+++ database/script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE cadastro_clientes;
+USE cadastro_clientes;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
